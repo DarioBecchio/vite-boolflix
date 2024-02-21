2989f18aa8e47258b7405165c9f84ebf0572f2a8
Dario Becchio <dabek1986@gmail.com>
2024-02-21 08:29:23 +0100

    Add API url

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
-<script >
-
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  data() {
+    return {
+      base_api_URL:
+        "https://api.themoviedb.org/3/search/movie?api_key=95637083a684a97bfbd4044fa4e72f18",
+    };
+  },
+  mounted() {
+    console.log(this.base_api_URL);
+    axios.get("").then((response) => {
+      console.log(response);
+    });
+  },
+};
 </script>
 
-<template>
-  
-</template>
+<template></template>
 
-<style>
+<style></style>
