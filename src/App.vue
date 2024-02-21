d24a9abf8f62c7962de854d91f00ca5a6ea599a9
Dario Becchio <dabek1986@gmail.com>
2024-02-21 10:29:18 +0100

    create loader

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,44 @@ export default {
     return {
       base_api_URL:
         "https://api.themoviedb.org/3/search/movie?api_key=95637083a684a97bfbd4044fa4e72f18",
+      movies: [],
+      loading: true,
+      error: false,
     };
   },
-  mounted() {
-    console.log(this.base_api_URL);
-    axios.get("").then((response) => {
-      console.log(response);
-    });
+  methods: {
+    getMovies(url) {
+      axios
+        .get(url)
+        .then((response) => {
+          console.log(response);
+          console.log(response.data.results);
+          this.movies = response.data.results;
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+  computed: {
+    getResults() {
+      return this.movies.data.results.length;
+    },
+  },
+  created() {
+    setTimeout(() => {
+      this.getMovies(this.base_api_URL);
+    }, 3000);
   },
 };
 </script>
 
-<template></template>
+<template>
+  <div class="row" v-if="loading"></div>
+  <div v-else>Loading...</div>
+  <div v-if="movies.results">{{ getResults() }}</div>
+  <div>{{ movies.results ? getResults : "No results" }}</div>
+</template>
 
 <style></style>
