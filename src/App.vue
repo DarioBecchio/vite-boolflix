8a86554a2267247b5caac4dcde1f36a94580cc8c
Dario Becchio <dabek1986@gmail.com>
2024-02-22 17:17:53 +0100

    CSS style

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,8 @@
 <script>
-import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 import { state } from "./state.js";
 export default {
-  name: "App",
-  data() {
-    return {
-      base_api_URL:
-        "https://api.themoviedb.org/3/search/movie?api_key=95637083a684a97bfbd4044fa4e72f18",
-      base_image_URL: "https://image.tmdb.org/t/p/w342",
-      movies: [],
-      searchKeyWord: "",
-      state,
-    };
-  },
-  methods: {
-    getMovies(url) {
-      axios
-        .get(url)
-        .then((response) => {
-          console.log(response);
-          console.log(response.data.results);
-          this.movies = response.data.results;
-          console.log(this.movies);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
-    getFilteredMovies() {
-      const url = `${this.base_api_URL}&query=${this.searchKeyWord}`;
-      console.log(url);
-      this.getMovies(url);
-    },
-  },
-
   components: { AppHeader, AppMain },
   computed: {
     getResults() {
@@ -51,14 +18,6 @@ export default {
 <template>
   <AppHeader></AppHeader>
   <AppMain></AppMain>
-  <div class="filter">
-    <input
-      type="text"
-      placeholder="Write a Movie Title Here"
-      v-model="searchKeyWord"
-    />
-    <button @click.enter="getFilteredMovies">Search Movies</button>
-  </div>
 
   <div class="card" v-for="movie in movies">
     <img :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`" alt="" />
